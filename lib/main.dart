231984bd12befdb2862cfa18ee7560e64f65abf4
牧野唯希 <sw23263n@st.omu.ac.jp>
2026-04-02 14:37:02 +0900

    アプリタイトルを「UWB App Mock」に変更し、テーマ設定を更新

--- lib/main.dart
+++ lib/main.dart
@@ -13,26 +13,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'UWB App Mock',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        primarySwatch: Colors.blue,
+        useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: UwbDistanceScreen(),
+      debugShowCheckedModeBanner: false, // 右上の「DEBUG」リボンを消す
     );
   }
 }
