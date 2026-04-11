a29901c6a536685a096a5921af693fba0f8ee9fa
yuikinman21 <yuikinman21@gmail.com>
2026-04-11 23:56:02 +0900

    不要なプラグインの登録を削除し、生成されたプラグインリストを更新

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-import share_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
 }
