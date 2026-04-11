da6c6678cfa83da89f54f51e364ac48c976008b1
yuikinman21 <yuikinman21@gmail.com>
2026-04-11 23:33:12 +0900

    SharePlusプラグインをインポート

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'dart:io';
 import 'package:intl/intl.dart';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:share_plus/share_plus.dart';
 
 void main() {
   runApp(const MyApp());
